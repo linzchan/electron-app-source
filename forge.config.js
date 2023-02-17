212ec438f99cd0bb3e7fd6d4aeea0aea19484891
linzChan <chenjinlin@xmiles.com>
2023-02-17 16:16:56 +0800

    init

---
+++ forge.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  packagerConfig: {
+    osxSign: {},
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID,
+    }
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'linzchan',
+          name: 'my-electron-app',
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
+};
